159e1c1b4071cd0162e1b1952684bdf9b2eef9b0
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2021-10-06 14:49:52 +0100

    Refactor: duplicate method.

--- four_key_metrics/jenkins.py
+++ four_key_metrics/jenkins.py
@@ -10,7 +10,34 @@ class Build:
         self.git_reference = git_reference
 
 class Jenkins:
-    pass
+    def get_jenkins_builds(host, job):
+        response = requests.get(
+            host +
+            "job/%s/api/json" % job,
+            params={
+                "tree": "builds[timestamp,result,duration,actions[parameters[*]],changeSet[items[*]]]"
+            },
+            headers={
+
+            },
+        )
+        body = response.json()
+
+        if len(body['builds']) == 0:
+            return []
+
+        builds = []
+        for build in body['builds']:
+            started_at = build['timestamp'] / 1000
+            builds.append(Build(
+                started_at=started_at,
+                finished_at=started_at + build['duration'] / 1000,
+                successful=build['result'] == 'SUCCESS',
+                environment=build['actions'][0]['parameters'][0]['value'],
+                git_reference=build['actions'][0]['parameters'][1]['value']
+            ))
+        return builds
+
 
 def get_jenkins_builds(host, job):
     response = requests.get(
