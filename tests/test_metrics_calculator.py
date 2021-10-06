9ed4e3e4d824bffc06f4f3191ed8663fceda8e74
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2021-10-06 12:21:59 +0100

    Add asserts for all tests.

--- tests/test_metrics_calculator.py
+++ tests/test_metrics_calculator.py
@@ -2,8 +2,9 @@ from four_key_metrics.metrics_calculator import MetricsCalculator
 
 
 def test_can_calculate_metrics_for_nothing():
-    assert MetricsCalculator().get_average_lead_time() is None
-    assert MetricsCalculator().get_lead_time_standard_deviation() is None
+    calculator = MetricsCalculator()
+    assert calculator.get_average_lead_time() is None
+    assert calculator.get_lead_time_standard_deviation() is None
 
 
 def test_can_calculate_metrics_for_one_deploy_with_one_commit():
@@ -21,6 +22,7 @@ def test_can_calculate_lead_time_for_one_deploy_with_two_commits():
     calculator.add_deploy(timestamp=10, commit_timestamps=[8, 8])
 
     assert calculator.get_average_lead_time() == 2
+    assert calculator.get_lead_time_standard_deviation() == 0
 
 
 def test_can_calculate_lead_time_for_two_deploys_with_two_commits():
@@ -30,3 +32,4 @@ def test_can_calculate_lead_time_for_two_deploys_with_two_commits():
     calculator.add_deploy(timestamp=20, commit_timestamps=[9, 9])
 
     assert calculator.get_average_lead_time() == 6.5
+    assert calculator.get_lead_time_standard_deviation() == 4.5
