8c91ad644789855b1fe3b659aa2cd00ae614edac
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2021-10-13 15:01:46 +0100

    Pass the right arguments through

--- four_key_metrics/use_case/get_lead_time_for_project.py
+++ four_key_metrics/use_case/get_lead_time_for_project.py
@@ -10,24 +10,23 @@ class GetLeadTimeForProject(object):
                  jenkins_job,
                  github_organisation,
                  github_repository):
-        if len(self.get_jenkins_builds('')) < 2:
+        jenkins_builds = self.get_jenkins_builds(jenkins_job)
+        if len(jenkins_builds) < 2:
             return {
                 'successful': False,
                 'lead_time_mean_average': None,
                 'lead_time_standard_deviation': None
             }
 
-        builds = self.get_jenkins_builds(job=None)
-
         commits = self.get_commits_between(
-            organisation=None,
-            repository=None,
-            base=None,
-            head=None
+            organisation=github_organisation,
+            repository=github_repository,
+            base=jenkins_builds[0].git_reference,
+            head=jenkins_builds[-1].git_reference
         )
 
         calculator = MetricsCalculator()
-        calculator.add_deploy(timestamp=builds[-1].finished_at, commit_timestamps=list(map(lambda x: x.timestamp, commits)))
+        calculator.add_deploy(timestamp=jenkins_builds[-1].finished_at, commit_timestamps=list(map(lambda x: x.timestamp, commits)))
 
         return {
             'successful': True,
--- tests/test_get_lead_time_for_project.py
+++ tests/test_get_lead_time_for_project.py
@@ -1,3 +1,5 @@
+import pytest
+
 from four_key_metrics.github import GitCommit
 from four_key_metrics.jenkins import Build
 from four_key_metrics.use_case.get_lead_time_for_project import GetLeadTimeForProject
@@ -38,7 +40,6 @@ def test_can_not_get_lead_time_for_one_build():
     assert not response['successful']
     assert response['lead_time_mean_average'] is None
     assert response['lead_time_standard_deviation'] is None
-
 
 
 def test_can_get_lead_time_for_two_builds_one_commit():
@@ -107,3 +108,84 @@ def test_can_get_lead_time_for_two_builds_two_commits():
     assert response['lead_time_mean_average'] == 2
     assert response['lead_time_standard_deviation'] == 0
 
+
+@pytest.mark.parametrize("job", [('no/builds'), 'no/builds2'])
+def test_can_pass_parameters_to_get_jenkins_builds_correctly(job):
+    spy_calls = []
+
+    def spy(job):
+        spy_calls.append(job)
+        return [Build(
+            started_at=0,
+            finished_at=1,
+            successful=True,
+            environment='Production',
+            git_reference='123456'
+        ), Build(
+            started_at=0,
+            finished_at=1,
+            successful=True,
+            environment='Production',
+            git_reference='123456'
+        )]
+
+    get_lead_time_for_project = GetLeadTimeForProject(
+        get_commits_between=lambda organisation, repository, base, head: [],
+        get_jenkins_builds=spy
+    )
+    get_lead_time_for_project(
+        jenkins_job=job,
+        github_organisation='has-no-commits',
+        github_repository='commit-less'
+    )
+
+    assert len(spy_calls) == 1
+    assert spy_calls[0] == job
+
+
+@pytest.mark.parametrize(
+    "head, base, organisation, repository",
+    [
+        ('b', 'a', 'has-no-commits', 'commit-less'),
+        ('c', 'd', 'another-org', 'commit-ful'),
+    ]
+)
+def test_can_pass_parameters_to_get_commits_between_correctly(head, base, organisation, repository):
+    builds = [
+        Build(
+            started_at=0,
+            finished_at=1,
+            successful=True,
+            environment='Production',
+            git_reference=base
+        ),
+        Build(
+            started_at=0,
+            finished_at=2,
+            successful=True,
+            environment='Production',
+            git_reference=head
+        )
+    ]
+
+    spy_calls = []
+
+    def spy(**kwargs):
+        spy_calls.append(kwargs)
+        return []
+
+    get_lead_time_for_project = GetLeadTimeForProject(
+        get_commits_between=spy,
+        get_jenkins_builds=lambda _: builds
+    )
+    get_lead_time_for_project(
+        jenkins_job='a/job',
+        github_organisation=organisation,
+        github_repository=repository
+    )
+
+    assert len(spy_calls) == 1
+    assert spy_calls[0]['head'] == head
+    assert spy_calls[0]['base'] == base
+    assert spy_calls[0]['organisation'] == organisation
+    assert spy_calls[0]['repository'] == repository
