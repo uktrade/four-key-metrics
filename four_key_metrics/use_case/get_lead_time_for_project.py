d9f09ebe92999a37af545f6359610775ca40c973
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2021-10-13 08:56:53 +0100

    Can get lead time for a really narrow case.

--- four_key_metrics/use_case/get_lead_time_for_project.py
+++ four_key_metrics/use_case/get_lead_time_for_project.py
@@ -1,3 +1,6 @@
+from four_key_metrics.metrics_calculator import MetricsCalculator
+
+
 class GetLeadTimeForProject(object):
     def __init__(self, get_commits_between, get_jenkins_builds):
         self.get_jenkins_builds = get_jenkins_builds
@@ -7,8 +10,28 @@ class GetLeadTimeForProject(object):
                  jenkins_job,
                  github_organisation,
                  github_repository):
+        if len(self.get_jenkins_builds('')) < 2:
+            return {
+                'successful': False,
+                'lead_time_mean_average': None,
+                'lead_time_standard_deviation': None
+            }
+
+        builds = self.get_jenkins_builds(job=None)
+
+        commits = self.get_commits_between(
+            organisation=None,
+            repository=None,
+            base=None,
+            head=None
+        )
+
+        calculator = MetricsCalculator()
+        calculator.add_deploy(timestamp=builds[-1].finished_at, commit_timestamps=list(map(lambda x: x.timestamp, commits)))
+
         return {
-            'successful': False,
-            'lead_time_mean_average': None,
-            'lead_time_standard_deviation': None
+            'successful': True,
+            'lead_time_mean_average': calculator.get_lead_time_mean_average(),
+            'lead_time_standard_deviation': calculator.get_lead_time_standard_deviation()
         }
+
--- tests/test_get_lead_time_for_project.py
+++ tests/test_get_lead_time_for_project.py
@@ -1,3 +1,5 @@
+from four_key_metrics.github import GitCommit
+from four_key_metrics.jenkins import Build
 from four_key_metrics.use_case.get_lead_time_for_project import GetLeadTimeForProject
 
 
@@ -14,3 +16,94 @@ def test_can_get_no_lead_time():
     assert not response['successful']
     assert response['lead_time_mean_average'] is None
     assert response['lead_time_standard_deviation'] is None
+
+
+def test_can_not_get_lead_time_for_one_build():
+    build = Build(
+        started_at=0,
+        finished_at=1,
+        successful=True,
+        environment='Production',
+        git_reference='123456'
+    )
+    get_lead_time_for_project = GetLeadTimeForProject(
+        get_commits_between=lambda organisation, repository, base, head: [],
+        get_jenkins_builds=lambda job: [build]
+    )
+    response = get_lead_time_for_project(
+        jenkins_job='no/builds',
+        github_organisation='has-no-commits',
+        github_repository='commit-less'
+    )
+    assert not response['successful']
+    assert response['lead_time_mean_average'] is None
+    assert response['lead_time_standard_deviation'] is None
+
+
+
+def test_can_get_lead_time_for_two_builds_one_commit():
+    build1 = Build(
+        started_at=0,
+        finished_at=1,
+        successful=True,
+        environment='Production',
+        git_reference='123456'
+    )
+    build2 = Build(
+        started_at=0,
+        finished_at=1,
+        successful=True,
+        environment='Production',
+        git_reference='123457'
+    )
+    commit = GitCommit(
+        timestamp=0
+    )
+    get_lead_time_for_project = GetLeadTimeForProject(
+        get_commits_between=lambda organisation, repository, base, head: [commit],
+        get_jenkins_builds=lambda job: [build1, build2]
+    )
+    response = get_lead_time_for_project(
+        jenkins_job='no/builds',
+        github_organisation='has-no-commits',
+        github_repository='commit-less'
+    )
+    assert response['successful']
+    assert response['lead_time_mean_average'] == 1
+    assert response['lead_time_standard_deviation'] == 0
+
+
+def test_can_get_lead_time_for_two_builds_two_commits():
+    build1 = Build(
+        started_at=0,
+        finished_at=1,
+        successful=True,
+        environment='Production',
+        git_reference='123456'
+    )
+    build2 = Build(
+        started_at=0,
+        finished_at=2,
+        successful=True,
+        environment='Production',
+        git_reference='123457'
+    )
+    commit1 = GitCommit(
+        timestamp=0
+    )
+    commit2 = GitCommit(
+        timestamp=0
+    )
+    get_lead_time_for_project = GetLeadTimeForProject(
+        get_commits_between=lambda organisation, repository, base, head: [commit1, commit2],
+        get_jenkins_builds=lambda job: [build1, build2]
+    )
+    response = get_lead_time_for_project(
+        jenkins_job='no/builds',
+        github_organisation='has-no-commits',
+        github_repository='commit-less'
+    )
+    assert response['successful']
+    assert response['lead_time_mean_average'] == 2
+    assert response['lead_time_standard_deviation'] == 0
+
