031948ef24a5c5e53c59b805c9ff6d76183ae62d
marijnkampf <marijn.kampf@digital.trade.gov.uk>
2022-03-28 12:15:55 +0100

    Ignore entries where no git reference present - which happens on releasing from non existing tag.

--- four_key_metrics/jenkins.py
+++ four_key_metrics/jenkins.py
@@ -14,59 +14,63 @@ class Build:
 
 
 def get_action(key, parameter_path, actions):
-    a = list(filter(lambda a: a.get('_class') == key, actions))
-    return glom(a, Path(0, *parameter_path))
+    a = list(filter(lambda a: a.get("_class") == key, actions))
+    if a:
+        return glom(a, Path(0, *parameter_path))
+    else:
+        return None
 
 
 class Jenkins:
-
     def __init__(self, host):
         self.host = host
 
     def get_jenkins_builds(self, job):
         response = requests.get(
-            self.host +
-            "job/%s/api/json" % job,
+            self.host + "job/%s/api/json" % job,
             params={
                 "tree": "builds["
-                        "timestamp,result,duration,"
-                        "actions["
-                        "parameters[*],"
-                        "lastBuiltRevision[branch[*]]"
-                        "],"
-                        "changeSet[items[*]]"
-                        "]"
+                "timestamp,result,duration,"
+                "actions["
+                "parameters[*],"
+                "lastBuiltRevision[branch[*]]"
+                "],"
+                "changeSet[items[*]]"
+                "]"
             },
-            auth=(os.environ['DIT_JENKINS_USER'], os.environ['DIT_JENKINS_TOKEN'])
+            auth=(os.environ["DIT_JENKINS_USER"], os.environ["DIT_JENKINS_TOKEN"]),
         )
         body = response.json()
 
-        if len(body['builds']) == 0:
+        if len(body["builds"]) == 0:
             return []
 
         builds = []
-        for build in body['builds']:
-            started_at = build['timestamp'] / 1000
-            builds.append(Build(
-                started_at=started_at,
-                finished_at=started_at + build['duration'] / 1000,
-                successful=build['result'] == 'SUCCESS',
-                environment=self._get_environment(build),
-                git_reference=self._get_git_reference(build)
-            ))
+        for build in body["builds"]:
+            started_at = build["timestamp"] / 1000
+            builds.append(
+                Build(
+                    started_at=started_at,
+                    finished_at=started_at + build["duration"] / 1000,
+                    successful=build["result"] == "SUCCESS",
+                    environment=self._get_environment(build),
+                    git_reference=self._get_git_reference(build),
+                )
+            )
 
-        return builds
+        # Ignore entries where no git reference present - which happens on releasing from non existing tag.
+        return [build for build in builds if build.git_reference]
 
     def _get_git_reference(self, build):
         return get_action(
-            'hudson.plugins.git.util.BuildData',
-            ['lastBuiltRevision', 'branch', 0, 'SHA1'],
-            build['actions']
+            "hudson.plugins.git.util.BuildData",
+            ["lastBuiltRevision", "branch", 0, "SHA1"],
+            build["actions"],
         )
 
     def _get_environment(self, build):
         return get_action(
-            'hudson.model.ParametersAction',
-            ['parameters', 0, 'value'],
-            build['actions']
+            "hudson.model.ParametersAction",
+            ["parameters", 0, "value"],
+            build["actions"],
         )
