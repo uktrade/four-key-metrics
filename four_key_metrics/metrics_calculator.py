bd00bc035c3cf407469907346fff9162cc18b20b
marijnkampf <marijn.kampf@digital.trade.gov.uk>
2022-03-29 16:39:04 +0100

    Investigation into data accuracy

--- four_key_metrics/metrics_calculator.py
+++ four_key_metrics/metrics_calculator.py
@@ -1,3 +1,6 @@
+from datetime import timedelta, datetime
+from pprint import pprint
+
 import statistics
 
 
@@ -8,17 +11,19 @@ class MetricsCalculator(object):
     def get_lead_time_mean_average(self):
         if self._no_deploys():
             return None
-
+        print("get_lead_time_mean_average")
         lead_times = self._get_lead_times()
+
         return sum(lead_times) / len(lead_times)
 
     def get_lead_time_standard_deviation(self):
         if self._no_deploys():
             return None
-
+        print("get_lead_time_standard_deviation")
         return statistics.pstdev(self._get_lead_times())
 
     def add_deploy(self, timestamp, commit_timestamps, commit_hash):
+        # print("COMIT_HASS:", commit_hash)
         if len(commit_timestamps) == 0:
             return
 
@@ -32,12 +37,33 @@ class MetricsCalculator(object):
 
     def _get_lead_times(self):
         lead_times = []
+        # pprint(self.deploys)
         for deploy in self.deploys:
+            print(
+                "COMMIT_HASH:",
+                deploy["commit_hash"],
+            )
             for commit_timestamp in deploy["commit_timestamps"]:
                 lead_time = deploy["timestamp"] - commit_timestamp
-                if lead_time > 8460000:
-                print("HASH: ", deploy["commit_hash"], "LEAD TIME: ", lead_time)
-                lead_times.append(deploy["timestamp"] - commit_timestamp)
+                lead_times.append(lead_time)
+                print(str(timedelta(seconds=lead_time)))
+        # #            print(
+        #  #               "deploy[timestamp]-commit",
+        #    #             datetime.fromtimestamp(deploy["timestamp"]).strftime("%c"),
+        #   #              datetime.fromtimestamp(commit_timestamp).strftime("%c"),
+        #                 "lead_time",
+        #                 str(timedelta(seconds=lead_time)),
+        #             )
+        # pprint(lead_times)
+        #       print(
+        ##            "deplo",
+        #          datetime.fromtimestamp(deploy["timestamp"]).strftime("%c"),
+        #            "lead times:",
+        #            lead_times,
+        #            len(lead_times),
+        #            str(timedelta(seconds=sum(lead_times) / len(lead_times))),
+        #     )
+        print()
         return lead_times
 
     def _no_deploys(self) -> bool:
