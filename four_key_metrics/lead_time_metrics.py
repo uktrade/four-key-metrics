ff73d921f05f349e5c232a9169dcb8822540510b
Vincent Farah <vincent.farah@madetech.com>
2022-04-22 16:31:24 +0100

    Refactor lead time metrics to so

--- four_key_metrics/lead_time_metrics.py
+++ four_key_metrics/lead_time_metrics.py
@@ -32,64 +32,68 @@ def generate_lead_time_metrics(projects):
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for project in projects:
-            response = all_builds.add_project(
-                jenkins_job=project[
-                    "job"
-                ],  # input("Jenkins job id (e.g. datahub-api)"),
-                github_organisation="uktrade",  # input("GitHub org (e.g. uktrade)"),
-                github_repository=project[
-                    "repository"
-                ],  # input("GitHub repository (e.g. data-hub-api)"),
-                environment=project[
-                    "environment"
-                ],  # input("Environment (e.g. production)")
-            )
-            if not response["successful"]:
-                pprint(
-                    {
-                        "project": project["repository"],
-                        "average": response["lead_time_mean_average"],
-                        "standard_deviation": response["lead_time_standard_deviation"],
-                    }
-                )
-            else:
-                for build in response["builds"]:
-                    build
-                    for commit in build.commits:
-                        writer.writerow(
-                            {
-                                "repository": project["repository"],
-                                "build_commit_hash": build.git_reference,
-                                "build_timestamp": build.finished_at,
-                                "build_time": datetime.fromtimestamp(
-                                    build.finished_at
-                                ).strftime("%d/%m/%Y %H:%M:%S"),
-                                "commit_hash": commit.sha,
-                                "commit_timestamp": commit.timestamp,
-                                "commit_time": datetime.fromtimestamp(
-                                    commit.timestamp
-                                ).strftime("%d/%m/%Y %H:%M:%S"),
-                                "commit_lead_time_days": commit.lead_time / 86400,
-                                "commit_lead_time": str(
-                                    timedelta(seconds=commit.lead_time)
-                                ),
-                                "previous_build_commit_hash": build.last_build_git_reference,
-                            }
-                        )
-
-                pprint(
-                    {
-                        "project": project["repository"],
-                        "environment": project["environment"],
-                        "average": str(
-                            timedelta(seconds=response["lead_time_mean_average"])
-                        ),
-                        "standard_deviation": str(
-                            timedelta(seconds=response["lead_time_standard_deviation"])
-                        ),
-                    },
-                    sort_dicts=False,
-                )
+        _write_metrics_for_projects(projects, all_builds, writer)
 
     pprint(f"Detailed metrics stored in {csv_filename}")
+
+
+def _write_metrics_for_projects(projects, all_builds, writer):
+    for project in projects:
+        response = all_builds.add_project(
+            jenkins_job=project["job"],  # input("Jenkins job id (e.g. datahub-api)"),
+            github_organisation="uktrade",  # input("GitHub org (e.g. uktrade)"),
+            github_repository=project[
+                "repository"
+            ],  # input("GitHub repository (e.g. data-hub-api)"),
+            environment=project[
+                "environment"
+            ],  # input("Environment (e.g. production)")
+        )
+        if not response["successful"]:
+            pprint(
+                {
+                    "project": project["repository"],
+                    "average": response["lead_time_mean_average"],
+                    "standard_deviation": response["lead_time_standard_deviation"],
+                }
+            )
+        else:
+            _write_build_metrics(writer, project, response)
+
+            pprint(
+                {
+                    "project": project["repository"],
+                    "environment": project["environment"],
+                    "average": str(
+                        timedelta(seconds=response["lead_time_mean_average"])
+                    ),
+                    "standard_deviation": str(
+                        timedelta(seconds=response["lead_time_standard_deviation"])
+                    ),
+                },
+                sort_dicts=False,
+            )
+
+
+def _write_build_metrics(writer, project, response):
+    for build in response["builds"]:
+        build
+        for commit in build.commits:
+            writer.writerow(
+                {
+                    "repository": project["repository"],
+                    "build_commit_hash": build.git_reference,
+                    "build_timestamp": build.finished_at,
+                    "build_time": datetime.fromtimestamp(build.finished_at).strftime(
+                        "%d/%m/%Y %H:%M:%S"
+                    ),
+                    "commit_hash": commit.sha,
+                    "commit_timestamp": commit.timestamp,
+                    "commit_time": datetime.fromtimestamp(commit.timestamp).strftime(
+                        "%d/%m/%Y %H:%M:%S"
+                    ),
+                    "commit_lead_time_days": commit.lead_time / 86400,
+                    "commit_lead_time": str(timedelta(seconds=commit.lead_time)),
+                    "previous_build_commit_hash": build.last_build_git_reference,
+                }
+            )
