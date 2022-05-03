6af98fbb1503c49ec9ab53c8408f4d20108d33ce
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-05-03 16:23:26 +0100

    Refactor: copy generate_lead_time_metrics.

--- four_key_metrics/use_case/generate_lead_time_metrics.py
+++ four_key_metrics/use_case/generate_lead_time_metrics.py
@@ -1,2 +1,19 @@
+import os
+
+from four_key_metrics.all_builds import AllBuilds
+from four_key_metrics.data_presenters import DataPresenter
+from four_key_metrics.lead_time_metrics import _write_metrics_for_projects
+
+
 class GenerateLeadTimeMetrics:
-    pass
+    def generate_lead_time_metrics(projects: dict, data_presenter: DataPresenter):
+        all_builds = AllBuilds(
+            os.getenv("DIT_JENKINS_URI", "https://jenkins.ci.uktrade.digital/")
+        )
+        try:
+            data_presenter.begin()
+            _write_metrics_for_projects(
+                projects=projects, all_builds=all_builds, data_presenter=data_presenter
+            )
+        finally:
+            data_presenter.end()
