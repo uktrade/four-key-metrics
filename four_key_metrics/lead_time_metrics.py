333a045f904f4966762478c5f22f893ec7dea9e7
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-05-03 16:27:11 +0100

    Refactor: Use the new use case class.

--- four_key_metrics/lead_time_metrics.py
+++ four_key_metrics/lead_time_metrics.py
@@ -1,23 +1,14 @@
 import os
 from datetime import timedelta, datetime
 from pprint import pprint
-from four_key_metrics.all_builds import AllBuilds
 from four_key_metrics.data_presenters import (
     DataPresenter,
 )
+from four_key_metrics.use_case.generate_lead_time_metrics import GenerateLeadTimeMetrics
 
 
 def generate_lead_time_metrics(projects: dict, data_presenter: DataPresenter):
-    all_builds = AllBuilds(
-        os.getenv("DIT_JENKINS_URI", "https://jenkins.ci.uktrade.digital/")
-    )
-    try:
-        data_presenter.begin()
-        _write_metrics_for_projects(
-            projects=projects, all_builds=all_builds, data_presenter=data_presenter
-        )
-    finally:
-        data_presenter.end()
+    return GenerateLeadTimeMetrics().generate_lead_time_metrics(projects, data_presenter)
 
 
 def _write_metrics_for_projects(projects, all_builds, data_presenter: DataPresenter):
