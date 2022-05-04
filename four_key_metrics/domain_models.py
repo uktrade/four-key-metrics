4196b537db7c99983046d046932dc10fdec90444
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-05-04 11:36:38 +0100

    Refactor: Extract into separate class

--- four_key_metrics/domain_models.py
+++ four_key_metrics/domain_models.py
@@ -2,6 +2,23 @@ import os
 import ciso8601
 import requests
 
+class GitHubCommits:
+    def get_commits_between(self, organisation, repository, base, head):
+        response = requests.get(
+            "https://api.github.com/repos/%s/%s/compare/%s...%s?per_page=10000"
+            % (organisation, repository, base, head),
+            auth=(os.environ["GITHUB_USERNAME"], os.environ["GITHUB_TOKEN"]),
+            headers={"Accept": "application/vnd.github.v3+json"},
+            timeout=30,
+        )
+
+        commits = []
+        for commit in response.json()["commits"]:
+            commit_author_date = commit["commit"]["author"]["date"]
+            timestamp = ciso8601.parse_datetime(commit_author_date).timestamp()
+            commits.append(GitCommit(sha=commit["sha"], timestamp=timestamp))
+        return commits
+
 
 class Build:
     def __init__(
@@ -21,19 +38,8 @@ class Build:
         self.last_build_git_reference = None
 
     def get_commits_between(self, organisation, repository, base, head):
-
-        response = requests.get(
-            "https://api.github.com/repos/%s/%s/compare/%s...%s?per_page=10000"
-            % (organisation, repository, base, head),
-            auth=(os.environ["GITHUB_USERNAME"], os.environ["GITHUB_TOKEN"]),
-            headers={"Accept": "application/vnd.github.v3+json"},
-            timeout=30,
-        )
-
-        for self.commit in response.json()["commits"]:
-            commit_author_date = self.commit["commit"]["author"]["date"]
-            timestamp = ciso8601.parse_datetime(commit_author_date).timestamp()
-            self.commits.append(GitCommit(sha=self.commit["sha"], timestamp=timestamp))
+        self.commits = GitHubCommits()\
+            .get_commits_between(organisation, repository, base, head)
         return self.commits
 
     def set_last_build_git_reference(self, last_build_git_reference):
