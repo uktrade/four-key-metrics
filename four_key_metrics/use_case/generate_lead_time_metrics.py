d7152aa94929125a7fbd3536b0110f56d99dc60f
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-05-04 11:50:07 +0100

    Refactor: Remove type

--- four_key_metrics/use_case/generate_lead_time_metrics.py
+++ four_key_metrics/use_case/generate_lead_time_metrics.py
@@ -29,7 +29,7 @@ class GenerateLeadTimeMetrics:
             os.getenv("DIT_JENKINS_URI", "https://jenkins.ci.uktrade.digital/")
         )
 
-    def __call__(self, projects: dict, presenter: GenerateLeadTimeMetricsPresenter):
+    def __call__(self, projects, presenter: GenerateLeadTimeMetricsPresenter):
         self._presenter = presenter
         try:
             self._presenter.begin()
