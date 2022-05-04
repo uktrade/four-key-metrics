f8e46579d4b83a11e4e0ad4a3fd2d4a2bb88827b
Craig J. Bass <1889973+craigjbass@users.noreply.github.com>
2022-05-04 12:03:06 +0100

    Refactor: Formatting

--- four_key_metrics/use_case_factory.py
+++ four_key_metrics/use_case_factory.py
@@ -6,11 +6,12 @@ from four_key_metrics.use_case.generate_lead_time_metrics import GenerateLeadTim
 
 class UseCaseFactory:
     def create(self, name):
-        jenkins = JenkinsBuilds(os.getenv("DIT_JENKINS_URI", "https://jenkins.ci.uktrade.digital/"))
         use_cases = {
             'generate_lead_time_metrics': GenerateLeadTimeMetrics(
                 ProjectSummariser(
-                    jenkins,
+                    JenkinsBuilds(
+                        os.getenv("DIT_JENKINS_URI", "https://jenkins.ci.uktrade.digital/")
+                    ),
                     GitHubCommits()
                 )
             )
