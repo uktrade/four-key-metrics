bdd2f81e218238581f3e3ebeed5a6093372287f9
marijnkampf <marijn.kampf@digital.trade.gov.uk>
2022-05-10 15:34:30 +0100

    Optimisation

--- tests/utilities.py
+++ tests/utilities.py
@@ -4,7 +4,6 @@ import pytest
 
 def get_filename_and_captured_outerr(capsys, prefix, filetype):
     captured = capsys.readouterr()
-    regex_match = prefix + "[0-9]{2}-[0-9]{2}-[0-9]{4}_[0-9]{6}." + filetype
-    regex_filename = regex_match
+    regex_filename = prefix + "[0-9]{2}-[0-9]{2}-[0-9]{4}_[0-9]{6}." + filetype
     filename = re.search(regex_filename, captured.out).group()
     return filename, captured
