dbfe797ba7433744980f30bb89a3a45878f538c1
marijnkampf <marijn.kampf@digital.trade.gov.uk>
2022-05-10 14:48:06 +0100

    Separated mean time to restore csv file tests

--- tests/test_mean_time_to_restore_csv_file.py
+++ tests/test_mean_time_to_restore_csv_file.py
@@ -1,5 +1,4 @@
 import os
-
 import httpretty
 import pytest
 
@@ -11,21 +10,13 @@ from four_key_metrics.presenters.mean_time_to_restore import (
     CSVDataPresenter,
 )
 from four_key_metrics.use_case_factory import UseCaseFactory
-
 from four_key_metrics.gateways import PingdomOutages
+from tests.utilities import get_filename_and_captured_outerr
 from tests.mock_pingdom_request import httpretty_checks, httpretty_summary_outage_p1
-from display import DisplayShell
 
 
-class ConsoleOnlyPresenter(ConsolePresenter):
-    def add(self, data: dict):
-        pass
-
-    def begin(self):
-        pass
-
-    def end(self):
-        pass
+def get_csv_filename_and_captured_outerr(capsys):
+    return get_filename_and_captured_outerr(capsys, "mean_time_to_restore_", "csv")
 
 
 @pytest.fixture(autouse=True)
@@ -37,30 +28,34 @@ def around_each():
     httpretty.disable()
 
 
-def test_mean_time_to_restore_output_failure(capsys):
-    httpretty_checks()
-    httpretty_summary_outage_p1()
-    check_names = ["Failing"]
-
-    UseCaseFactory().create("generate_mean_time_to_restore")(
-        check_names, ConsoleOnlyPresenter()
-    )
-
-    captured = capsys.readouterr()
-    assert "'source': 'pingdom'" in captured.out
-    assert "'average': None" in captured.out
-    assert "'count': None" in captured.out
-
-
-def test_mean_time_to_restore_output(capsys):
+def test_csv_created(capsys):
     httpretty_checks()
     httpretty_summary_outage_p1()
     check_names = ["Data Hub P1"]
 
     UseCaseFactory().create("generate_mean_time_to_restore")(
-        check_names, ConsoleOnlyPresenter()
+        check_names, CSVDataPresenter.create()
     )
 
+    csv_filename, captured = get_csv_filename_and_captured_outerr(capsys)
+    file_exists = os.path.exists(csv_filename)
+    assert "CSV metrics stored in mean_time_to_restore_" in captured.out
+    assert file_exists
+    if file_exists:
+        os.remove(csv_filename)
+        assert os.path.exists(csv_filename) is False
+
+
+def test_mean_time_to_restore_csv(capsys):
+    httpretty_checks()
+    httpretty_summary_outage_p1()
+    check_names = ["Data Hub P1"]
+
+    UseCaseFactory().create("generate_mean_time_to_restore")(
+        check_names, CSVDataPresenter.create()
+    )
+
+    # Check
     captured = capsys.readouterr()
     assert "'source': 'pingdom'" in captured.out
     assert "'average': 1980" in captured.out
